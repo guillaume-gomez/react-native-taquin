60ed7ca0c00fbdb056ce42f76ccd3b14483e575d
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-09-30 09:10:03 +0200

    add new screen

---
+++ src/routes/DetailsScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styles from "../../styles";
+
+const DetailScreen : React.FunctionComponent = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+};
+
+
+export default DetailScreen;
