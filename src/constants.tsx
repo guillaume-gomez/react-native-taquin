a10d727912b2a49598d559e2620aec8ce71b0059
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-10-05 20:50:27 +0200

    export constants

---
+++ src/constants.tsx
@@ -0,0 +1,5 @@
+export const DEFAULT_IMAGE_URL = '../assets/images/puzzle-default.jpg';
+export const DEFAULT_PUZZLE_SIZE = 4;
+export const IMAGE_URLS = ['../assets/images/img1.jpg', '../assets/images/img2.jpg'];
+export const DURATION_TRANSLATE = 200;
+export const SIZE_TILE = 5.5;
