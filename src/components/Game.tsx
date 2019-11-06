4b266d871a94808d2fb7ad2181ad1c6df5097a66
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-07 00:03:02 +0100

    display the grid properly

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  Button
+} from 'react-native';
+
+import { 
+  DEFAULT_IMAGE_URL,
+  IMAGE_URLS
+} from '../constants';
+
 
 import Grid from './Grid';
 
-import withLoader from '../withLoader';
+import { buildGrid } from "../gameEngine/game";
+import { choiceInArray } from '../gameEngine/helper';
+import { splitImagesIntoTiles } from "../gameEngine/spriteGeneratorModule";
 
-import Button from '../ui/Button';
-import Switch from '../ui/Switch';
-const LoaderButton = withLoader(Button);
+
+import withLoader from '../withLoader';
+//import Button from '../ui/Button';
+//import Switch from '../ui/Switch';
+//const LoaderButton = withLoader(Button);
 
 //import { suggestFactory } from '../../services/suggestMoveService';
 
@@ -17,24 +32,38 @@ const labels = {
 };
 
 interface GameInterface {
-  currentGrid: Array;
-  isVictory: boolean;
-  onClickTile: () => void;
-  resolvedGrid: Array;
-  suggestedTile: number;
+  isVictory?: boolean;
+  onClickTile?: () => void;
+  suggestedTile?: number;
 }
 
 
 const Game : React.FunctionComponent<GameInterface> = ({
-  currentGrid,
   isVictory,
   onClickTile,
-  resolvedGrid,
   suggestedTile
 }) => {
 
   const [ showTileNumbers, setShowTileNumbers ] = useState(false);
   const [ loadingAdvice, setLoadingAdvice ] = useState(false);
+  const [ loadingSprites, setLoadingSprites ] = useState(true);
+  const [ imageUrls, setImageUrls] = useState([]);
+
+  const nbCellRow = 4;
+
+  async function generateSprite(imageUrl: string) {
+    const urls = await splitImagesIntoTiles(4, 500, imageUrl);
+    setImageUrls(urls);
+    setLoadingSprites(false);
+  }
+
+  useEffect(() => {
+    const imageUrl = choiceInArray(
+      IMAGE_URLS.length > 0 ? IMAGE_URLS : [DEFAULT_IMAGE_URL],
+    );
+    generateSprite(imageUrl);
+  }, []);
+
 
     // requestSuggest = async () => {
     //     const { currentGrid, resolvedGrid } = this.state;
@@ -61,22 +90,28 @@ const Game : React.FunctionComponent<GameInterface> = ({
       return;
     }
     //this.requestSuggest();
-  };
+  }
 
+  const grid = buildGrid(nbCellRow);
+
+  if(!!loadingSprites) {
+    return (<View><Text>Load sprites</Text></View>);
+  }
 
   return (
-    <div>
-      {currentGrid && (
-        <Grid
+    <View>
+      {grid && (
+         <Grid
             onClick={onClickTile}
-            grid={currentGrid}
-            readOnly={isVictory}
-            resolvedGrid={resolvedGrid}
-            showNumbers={showTileNumbers}
-            tileToHighLight={suggestedTile}
+            grid={grid}
+            readOnly={true}
+            resolvedGrid={[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]}
+            imageUrls={imageUrls}
+            showNumbers={false}
+            tileToHighLight={2}
         />
       )}
-      <Switch labels={labels} onToggle={handleOnToggle} />
+     {/* <Switch labels={labels} onToggle={handleOnToggle} />
       {!isVictory && (
         <div className="center">
           <LoaderButton
@@ -87,8 +122,8 @@ const Game : React.FunctionComponent<GameInterface> = ({
               onClick={handleClickSuggest}
           />
         </div>
-      )}
-    </div>
+      )}*/}
+    </View>
   );
 };
 
--- src/routes/GameScreen.tsx
+++ src/routes/GameScreen.tsx
@@ -1,61 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
-  View,
-  Text,
-  Button,
-  Platform
+  View
 } from 'react-native';
 
 import styles from "../../styles";
 
-import { 
-  DEFAULT_IMAGE_URL,
-  IMAGE_URLS
-} from '../constants';
 import Grid from "../components/Grid";
-import { buildGrid } from "../gameEngine/game";
-import { choiceInArray } from '../gameEngine/helper';
-import { splitImagesIntoTiles } from "../gameEngine/spriteGeneratorModule";
+import Game from "../components/Game";
 
 const GameScreen : React.FunctionComponent = ({navigation}) => {
-
-  const [imageUrls, setImageUrls] = useState([]);
 
   function onClickTile() {
     console.log("coucou");
   }
 
-  function onClick() {
-    return "";
-  }
-
-  const nbCellRow = 4;
-
-  async function generateSprite(imageUrl: string) {
-    const imageUrls = await splitImagesIntoTiles(4, 500, imageUrl);
-    setImageUrls(imageUrls);
-  }
-
-   useEffect(() => {
-    const imageUrl = choiceInArray(
-      IMAGE_URLS.length > 0 ? IMAGE_URLS : [DEFAULT_IMAGE_URL],
-    );
-    generateSprite(imageUrl);
-    
-  }, []);
-
-  const grid = buildGrid(nbCellRow);
-
   return (
     <View style={styles.container}>
-       <Grid
-          onClick={onClickTile}
-          grid={grid}
-          readOnly={true}
-          resolvedGrid={[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]}
-          imageUrls={imageUrls}
-          showNumbers={false}
-          tileToHighLight={2}
+       <Game
+          isVictory={false}
+          onClickTile={onClickTile}
+          suggestedTile={-1}
       />
     </View>
   );
