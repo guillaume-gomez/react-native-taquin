953fec756b6fe2d522c4a3fad0afe159dbb595ae
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-08 22:27:40 +0100

    add new images

--- src/constants.tsx
+++ src/constants.tsx
@@ -3,7 +3,7 @@ export const DURATION_TRANSLATE = 200;
 export const SIZE_TILE = 5.5;
 
 
-export const IMAGE_URLS = [require('../assets/images/img1.png'), require('../assets/images/img2.png')];
+export const IMAGE_URLS = [require('../assets/images/img1.png'), require('../assets/images/img2.png'), require('../assets/images/opensource.png')];
 export const DEFAULT_IMAGE_URL = require('../assets/images/puzzle-default.jpg');
 
 export const TOTO = require("../assets/images/img2.png");
